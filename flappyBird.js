333e84514367367e3b26bfd1247c1c50831975e8
ThatGuyAid3n <83622490+Aid3n-cmd@users.noreply.github.com>
2021-05-06 09:28:06 -0400

    Update flappyBird.js

--- flappyBird.js
+++ flappyBird.js
@@ -36,6 +36,15 @@ var scor = new Audio();
 fly.src = "sounds/fly.mp3";
 scor.src = "sounds/score.mp3";
 
+// on key down
+
+document.addEventListener("keydown",moveUp);
+
+function moveUp(){
+    bY -= 25;
+    fly.play();
+}
+
 // pipe coordinates
 
 var pipe = [];
